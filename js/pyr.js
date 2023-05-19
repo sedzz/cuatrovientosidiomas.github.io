ce7a63e746f9fa575b5176d9236a40120f190cf7
Sebas Diez <infernus10456ps3d@gmail.com>
2023-05-19 12:49:30 +0200

    Co-authored-by: Unai5 <Unai5@users.noreply.github.com> Co-authored-by: alexmele26 <alexmele26@users.noreply.github.com>

--- js/pyr.js
+++ js/pyr.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var containers = document.querySelectorAll("[id^='container']");
-    containers.forEach(function(container) {
-      container.querySelector("[id^='clickable']").addEventListener("click", toggle);
-    });
+  var containers = document.querySelectorAll(".pyrContainer");
+  containers.forEach(function(container) {
+    container.querySelector(".clickable").addEventListener("click", toggle);
   });
+});
 
-  function toggle() {
-    var content = this.nextElementSibling;
-    if (content.style.display === "none") {
-      content.style.display = "block";
-    } else {
-      content.style.display = "none";
-    }
+function toggle() {
+  var content = this.nextElementSibling;
+  if (content.style.display === "none") {
+    content.style.display = "block";
+  } else {
+    content.style.display = "none";
   }
+}
