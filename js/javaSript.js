ad8667374b50afc1dca0d8fdf1da0f0ce720479a
Sebas Diez <infernus10456ps3d@gmail.com>
2023-05-21 14:57:03 +0200

    Co-authored-by: alexmele26 <alexmele26@users.noreply.github.com>

--- js/javaSript.js
+++ js/javaSript.js
@@ -3,10 +3,11 @@
 /*UNAI*/
 
 
-function mostrarClaseGratis(){
-      document.getElementById('frmClaseGratis').style.display = 'block';
+function mostrarClaseGratis() {
+  document.getElementById('frmClaseGratis').style.display = 'block';
 }
-function ocultarClaseGratis(){
+
+function ocultarClaseGratis() {
   document.getElementById('frmClaseGratis').style.display = 'none';
 }
 
@@ -78,86 +79,3 @@ document.addEventListener('DOMContentLoaded', () => {
     e.preventDefault();
     window.location.href = this.href;
   });
-
-
-    /*VALIDACIÓN FORMULARIO*/
-  
-    window.addEventListener("DOMContentLoaded", function() {
-      var form = document.getElementById("myForm");
-      form.addEventListener("submit", function(event) {
-        event.preventDefault(); // Evitar el envío del formulario por defecto
-        if (validateForm()) {
-          form.submit(); // Envía el formulario si pasa la validación
-        }
-      });
-    });
-  
-    function validateForm() {
-      // Obtener los valores de los campos
-      var name = document.getElementById("name").value;
-      var dni = document.getElementById("dni").value;
-      var dob = document.getElementById("dob").value;
-      var email = document.getElementById("e").value;
-      var phone = document.getElementById("phone").value;
-      var language = document.getElementById("language").value;
-      var level = document.getElementById("level").value;
-      var experience = document.querySelector('input[name="experience"]:checked');
-      var objective = document.getElementById("objective").value;
-      
-      // Validar que todos los campos estén completos
-      if (name === "" || dni === "" || dob === "" || email === "" || phone === "" || language === "" || level === "" || experience === null || objective === "") {
-        alert("Por favor, completa todos los campos del formulario.");
-        return false;
-      }
-      
-      // Validar que el nombre solo tenga letras y un mínimo de 3 letras
-      var nameRegex = /^[a-zA-Z]{3,}$/;
-      if (!name.match(nameRegex)) {
-        alert("Por favor, ingresa un nombre válido con un mínimo de 3 letras y sin caracteres especiales.");
-        return false;
-      }
-      
-      // Validar que el formato del DNI sea 8 números y una letra
-      var dniRegex = /^\d{8}[a-zA-Z]$/;
-      if (!dni.match(dniRegex)) {
-        alert("Por favor, ingresa un DNI válido en el formato 12345678X.");
-        return false;
-      }
-      
-      // Validar que la fecha de nacimiento no sea superior a hoy ni hoy tampoco
-      var today = new Date();
-      var selectedDate = new Date(dob);
-      if (selectedDate >= today) {
-        alert("Por favor, ingresa una fecha de nacimiento válida.");
-        return false;
-      }
-      
-      // Validar el formato del correo electrónico (mismo código del ejemplo anterior)
-      var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      if (!email.match(emailRegex)) {
-        alert("Por favor, ingresa un correo electrónico válido.");
-        return false;
-      }
-      
-      // Validar que el teléfono tenga 9 números
-      var phoneRegex = /^\d{9}$/;
-      if (!phone.match(phoneRegex)) {
-        alert("Por favor, ingresa un número de teléfono válido de 9 dígitos.");
-        return false;
-      }
-      
-      // Validar que se haya seleccionado un idioma de interés (mismo código del ejemplo anterior)
-      if (language === "") {
-        alert("Por favor, selecciona un idioma de interés.");
-        return false;
-      }
-      
-      // Validar que se haya seleccionado un nivel de conocimiento (mismo código del ejemplo anterior)
-      if (level === "") {
-        alert("Por favor, selecciona un nivel de conocimiento.");
-        return false;
-      }
-      
-      // Si todos los campos son válidos, el formulario se puede enviar
-      return true;
-    }
---
+++ js/validaciones.js
@@ -0,0 +1,96 @@
+const nombreInput = document.getElementById('name');
+nombreInput.addEventListener('input', validarNombre);
+
+function validarNombre() {
+  const nombre = nombreInput.value;
+  const regex = /^[a-zA-Z][a-zA-Z\s]*$/;
+  if (!regex.test(nombre)) {
+    nombreInput.setCustomValidity('El nombre solo puede contener letras');
+    nombreInput.reportValidity();
+    document.querySelector('button[type="submit"]').disabled = true;
+  } else {
+    nombreInput.setCustomValidity('');
+    document.querySelector('button[type="submit"]').disabled = false;
+  }
+}
+
+  const dniInput = document.getElementById('dni');
+  dniInput.addEventListener('input', validarDNI);
+
+  function validarDNI() {
+    const dni = dniInput.value.trim().toUpperCase();
+    const regex = /^[0-9]{8}[A-Z]$/;
+    if (!regex.test(dni)) {
+      dniInput.setCustomValidity('El DNI debe contener 8 números seguidos de una letra mayúscula.');
+      dniInput.reportValidity();
+      document.querySelector('button[type="submit"]').disabled = true;
+    } else {
+      dniInput.setCustomValidity('');
+      document.querySelector('button[type="submit"]').disabled= false;
+    }
+  }
+
+  const fechaInput = document.getElementById('fecha');
+  fechaInput.addEventListener('input', validarFecha);
+
+  function validarFecha() {
+    const fecha = new Date(fechaInput.value);
+    const hoy = new Date();
+    if (fecha > hoy) {
+      fechaInput.setCustomValidity('La fecha debe ser menor o igual a hoy.');
+      fechaInput.reportValidity();
+      document.querySelector('button[type="submit"]').disabled = true;
+    } else {
+      fechaInput.setCustomValidity('');
+      document.querySelector('button[type="submit"]').disabled = false;
+    }
+  }
+
+  const emailInput = document.getElementById('email');
+  emailInput.addEventListener('input', validarEmail);
+
+  function validarEmail() {
+    const email = emailInput.value.trim();
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!regex.test(email)) {
+      emailInput.setCustomValidity('El correo electrónico debe tener un formato válido.');
+      emailInput.reportValidity();
+      document.querySelector('button[type="submit"]').disabled = true;
+    } else {
+      emailInput.setCustomValidity('');
+      document.querySelector('button[type="submit"]').disabled = false;
+    }
+  }
+  const idiomaSelect = document.getElementById('language');
+  idiomaSelect.addEventListener('change', deshabilitarOpcionSinValor);
+  
+  const nivelSelect = document.getElementById('level');
+  nivelSelect.addEventListener('change', deshabilitarOpcionSinValor);
+  
+  function deshabilitarOpcionSinValor() {
+    const opcionSeleccionadaIdioma = idiomaSelect.value;
+    const opcionSeleccionadaNivel = nivelSelect.value;
+  
+    // Deshabilitar la opción "Seleccionar idioma" si no tiene valor
+    idiomaSelect.options[0].disabled = opcionSeleccionadaIdioma === '';
+  
+    // Deshabilitar la opción "Seleccionar nivel" si no tiene valor
+    nivelSelect.options[0].disabled = opcionSeleccionadaNivel === '';
+  }
+
+  const telefonoInput = document.getElementById('phone');
+  telefonoInput.addEventListener('input', validarTelefono);
+  
+  function validarTelefono() {
+    const telefono = telefonoInput.value.trim();
+    const regex = /^[0-9]{9}$/;
+    
+    if (!regex.test(telefono)) {
+      telefonoInput.setCustomValidity('El número de teléfono debe contener solo números y tener una longitud de 9 dígitos.');
+      telefonoInput.reportValidity();
+      document.querySelector('button[type="submit"]').disabled = true;
+    } else {
+      telefonoInput.setCustomValidity('');
+      document.querySelector('button[type="submit"]').disabled = false;
+    }
+  }
